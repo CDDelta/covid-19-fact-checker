de870768d14399b24c529360c9671255903c4b8b
CDDelta <ngjunyen@gmail.com>
2020-04-13 14:19:44 +1000

    add sign in validation allow submit from keyboard

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, Input } from '@angular/core';
 import { PlatformLocation } from '@angular/common';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { MatInput } from '@angular/material/input';
+import { FormBuilder, Validators } from '@angular/forms';
 
 enum SignInState { Waiting = 'waiting', SendingEmail = 'sending_email', SentEmail = 'sent_email' };
 
@@ -11,29 +12,30 @@ enum SignInState { Waiting = 'waiting', SendingEmail = 'sending_email', SentEmai
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  public form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
   public state = SignInState.Waiting;
 
-  @ViewChild('emailInput', { read: MatInput })
-  private emailInput: MatInput;
-
-  constructor(private auth: AngularFireAuth, private location: PlatformLocation) { }
+  constructor(private auth: AngularFireAuth, private location: PlatformLocation, private fb: FormBuilder) { }
 
   async sendSignInEmail(): Promise<void> {
     this.state = SignInState.SendingEmail;
 
     try {
-      await this.auth.sendSignInLinkToEmail(this.emailInput.value, {
+      const email = this.form.value.email;
+
+      await this.auth.sendSignInLinkToEmail(email, {
         url: `${this.location.protocol}//${this.location.hostname}/account/callback`,
         handleCodeInApp: true,
       });
 
-      localStorage.setItem('signInEmail', this.emailInput.value);
+      localStorage.setItem('signInEmail', email);
     } catch {
       this.state = SignInState.Waiting;
     }
 
     this.state = SignInState.SentEmail;
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +11,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   declarations: [],
   exports: [
     CommonModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
