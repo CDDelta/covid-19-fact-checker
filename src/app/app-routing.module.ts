e5a05e6e16c120122b2100fad7319b41fb6b6826
CDDelta <ngjunyen@gmail.com>
2020-04-13 13:39:19 +1000

    fix sign in redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
-const redirectUnauthorizedToSignIn = () => redirectUnauthorizedTo(['sign-in']);
+const redirectUnauthorizedToSignIn = () => redirectUnauthorizedTo(['account', 'sign-in']);
 
 const routes: Routes = [
   {
